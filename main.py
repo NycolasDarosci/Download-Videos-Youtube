690d46a833ad7f8f095fba728ed0af89f2161604
Nycolas Darosci <nycoollaas@gmail.com>
2024-01-04 22:58:46 -0300

    some features

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from pytube import YouTube
 
+filesize = []
+
 def Download(link):
     youtubeObject = YouTube(link)
     youtubeObject = youtubeObject.streams.get_highest_resolution()
+    filesize.append(youtubeObject.filesize_mb)
     try:
+        print("downloading: ", youtubeObject.title, ", size: ", youtubeObject.filesize_mb, "MB")
         youtubeObject.download()
     except:
         print("An error has occurred")
-    print("Download is completed successfully")
+    print("Download was completed successfully")
 
 
-link = input("Enter the YouTube video URL: ")
-Download(link)
+links = []
+for l in links: 
+    Download(l)
+
+print("Total size downloaded: ", sum(filesize), "MB")
+filesize.clear()
